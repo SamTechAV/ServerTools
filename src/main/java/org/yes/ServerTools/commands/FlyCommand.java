f0a9a9b95b12a27463836ee8c73a40988982b8bd
SamTechAV <sam@sam-tech.net>
2024-09-24 01:38:50 -0400

    v2.7 Updated flight

--- src/main/java/org/yes/ServerTools/commands/FlyCommand.java
+++ src/main/java/org/yes/ServerTools/commands/FlyCommand.java
@@ -3,6 +3,7 @@ package org.yes.ServerTools.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.yes.ServerTools.ServerTools;
 import org.yes.ServerTools.utils.MessageUtil;
@@ -10,22 +11,25 @@ import org.yes.ServerTools.utils.MessageUtil;
 public class FlyCommand implements CommandExecutor {
 
     private final ServerTools plugin;
+    private final FileConfiguration config;
 
     public FlyCommand(ServerTools plugin) {
         this.plugin = plugin;
+        this.config = plugin.getConfig();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         if (!(sender instanceof Player)) {
-            sender.sendMessage(MessageUtil.parseMessage(sender, plugin.getConfig().getString("fly.playerOnly", "<red>This command can only be used by players!")));
+            sender.sendMessage(MessageUtil.parseMessage(sender, config.getString("fly.playerOnly", "<red>This command can only be used by players!"), null));
             return true;
         }
 
         Player player = (Player) sender;
 
         if (!player.hasPermission("servertools.fly")) {
-            player.sendMessage(MessageUtil.parseMessage(player, plugin.getConfig().getString("fly.noPermission", "<red>You don't have permission to use this command.")));
+            player.sendMessage(MessageUtil.parseMessage(player, config.getString("fly.noPermission", "<red>You don't have permission to use this command."), null));
             return true;
         }
 
@@ -34,10 +38,10 @@ public class FlyCommand implements CommandExecutor {
         player.setFlying(isFlying);
 
         String message = isFlying
-                ? plugin.getConfig().getString("fly.enabled", "<green>Flight mode enabled.")
-                : plugin.getConfig().getString("fly.disabled", "<red>Flight mode disabled.");
+                ? config.getString("fly.enabled", "<green>Flight mode enabled.")
+                : config.getString("fly.disabled", "<red>Flight mode disabled.");
 
-        player.sendMessage(MessageUtil.parseMessage(player, message));
+        player.sendMessage(MessageUtil.parseMessage(player, message, null));
 
         return true;
     }
